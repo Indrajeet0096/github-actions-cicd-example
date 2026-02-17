fcc3a3b7192342a0d4ea0b192b47726fedb64108
Indrajeet2112 <indrajeetsanas24@gmail.com>
2026-02-17 09:55:26 +0530

    Add CI/CD workflow

---
+++ src/app.test.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+const request = require('supertest');
+test('GET / returns Hello CI/CD!', async () => {
+const response = await request(app).get('/');
+expect(response.text).toBe('Hello CI/CD!');
+});
